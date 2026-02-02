1fc0a316a58fbb0d196c62b56649cf0fb721cd25
V_Rishivardan <rishivardan2004@gmail.com>
2026-02-02 23:42:32 +0530

    Flask Introduction
    Flask Basics integrating with html website and mapping to local host

---
+++ app.py
@@ -0,0 +1,12 @@
+#Basic Flask application setup
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
